123c9a8fc5fb77a842f5a98f4709f78fb72449ac
Patrik17671 <patrik.spilka@gmail.com>
2022-10-29 19:27:35 +0200

    Tailwind and SASS added

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 const Product = ({product}) => {
 	//Extract data
 	const {title, price, images} = product.attributes
@@ -6,10 +8,14 @@ const Product = ({product}) => {
 			<div>
 				<img src={images.data[0].attributes.formats.small.url} alt=""/>
 			</div>
-			<h2>{title}</h2>
-			<h3>{price}</h3>
+			<h2 className="">{title}</h2>
+			<PriceStyle>{price}</PriceStyle>
 		</div>
 	);
 };
 
 export default Product;
+
+const PriceStyle = styled.h3`
+	color: red;
+`
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/main.sass'
+
 import {Provider, createClient} from "urql";
 
 const client = createClient({url: process.env.NEXT_PUBLIC_BACKEND_API})
