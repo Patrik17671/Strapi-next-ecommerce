5df384ed1c9eb678c1016658ff5d58b046129d02
Patrik17671 <patrik.spilka@gmail.com>
2022-11-14 18:50:36 +0100

    Auth0 login added

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import Cart from "./Cart";
 import {useStateContext} from "../lib/context";
+import User from "./User";
+import {useUser} from "@auth0/nextjs-auth0";
 
 export default function Nav(){
 	
 	const {showCart, setShowCart, totalQty} = useStateContext();
+	const {user, error, isLoading} = useUser();
+	console.log(useUser())
 	
 	return(
 		<header>
@@ -12,9 +16,9 @@ export default function Nav(){
 				<nav>
 					<Link href={"/"}>homee</Link>
 					<div>
+						<User />
 						<div onClick={() => setShowCart(true)}>
 							<h3>Košík <sup>{totalQty > 0 && totalQty}</sup></h3>
-							
 						</div>
 					</div>
 				</nav>
